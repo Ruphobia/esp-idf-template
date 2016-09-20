edd92f34d9b81e12b0d297bf57599dbe206f706e
Jerry Woods <whophlungpoo@yahoo.com>
2016-09-20 17:05:02 -0400

    lil mods to template app

---
+++ main/headers.h
@@ -0,0 +1,12 @@
+#ifndef _PROJECTHEADERS_H_
+#define	_PROJECTHEADERS_H_
+
+#include <stdio.h>
+#include <freertos/FreeRTOS.h>
+#include <freertos/task.h>
+#include <freertos/portmacro.h>
+#include <freertos/FreeRTOSConfig.h>
+#include <stdlib.h>
+#include "sdkconfig.h"
+
+#endif
